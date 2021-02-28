723208457aa3035982968fed07a40f2f9e18d0ef
TJ Edwards <7.3dward5@gmail.com>
2021-02-28 01:30:07 -0500

    All fixed

--- blockchain-tj.ps1
+++ blockchain-tj.ps1
@@ -2,14 +2,14 @@ class Transaction {
     [String]$fromAddress;
     [String]$toAddress;
     #Add type: $Type;
-    [Int32]$ammount;
+    [Int]$ammount;
 
-    Transaction ([String]$fromAddress, [String]$toAddress, [Int32]$ammount) {
+    Transaction ([String]$fromAddress, [String]$toAddress, [Int]$ammount) {
         $this.fromAddress = $fromAddress
         $this.toAddress = $toAddress
         $this.ammount = $ammount
     }
-
+    # To give a string output to the class
     [String] ToString() {
         return "from = $($this.fromAddress), to = $($this.toAddress), ammount = $($this.ammount)"
     }
@@ -30,10 +30,19 @@ class Block {
         $this.timestamp = $timestamp
         
     }
+    # To give a string output to the class
+    [String]ToString(){
+        return "nonce: $($this.nonce), hash: $($this.hash), previousHash: $($this.hash_prev)"
+    }
     [String] calculateHash(){
-        Write-Host "Transaction: $($this.transaction.ToString())"
-        $stream = "$($this.hash_prev) + $($this.timestamp.ToString()) + $($this.transaction.ToString())"
-        return Get-FileHash -InputStream $([IO.MemoryStream]::new([byte[]][char[]]$stream)) -Algorithm SHA256
+        # Input string to hash
+        $string = "$($this.hash_prev)$($this.timestamp.ToString())$($this.transaction.ToString())"
+
+        #Hash it up
+        $hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+        $string = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($string))
+        $hashString = [System.BitConverter]::ToString($string)
+        return $hashString.Replace('-', '')
     }
     mineBlock([Int32]$difficulty) {
         while ($($this.hash.Substring(0, $difficulty)).length -ne (([char[]]$difficulty).Length + 1) ){
@@ -44,9 +53,9 @@ class Block {
 }
 
 class BlockChain {
-    [Block[]]$chain = @()
+    [Block[]]$chain
     [Int32]$difficulty = 2
-    [Transaction[]]$pendingTransactions = @()
+    [Transaction[]]$pendingTransactions
     [Int32]$minningReward = 100
     
     BlockChain(){
@@ -58,22 +67,24 @@ class BlockChain {
     }
         
     minePendingTransactions($minningRewardAddress) {
-        $block = $null
 
-        foreach ( $transactions in $this.pendingTransactions ){ 
-            $block = [Block]::new($(Get-Date), $transactions, $this.chain[$this.chain.lenth -1].hash)
-        }
-        $block.mineBlock($this.difficulty)
-
-        if ($?) { Write-Host "Blocks Successfully Mined!" }
-        $this.chain += $block
-
-        $this.pendingTransactions += [Transaction]::new("RewardAddress", $minningRewardAddress, $this.minningReward)
+        foreach ( $transaction in $this.pendingTransactions ){ 
+            
+            Write-Host "Processing transaction: $($transaction.ToString())..."
+            $block = [Block]::new($(Get-Date), $transaction, $($this.chain[$this.chain.lenth -1].hash))
+            Write-Host "Created block: $($block.ToString())"
+            $block.mineBlock($this.difficulty)
+            if ($?) { Write-Host "*** $($this.pendingTransactions.length) Blocks Successfully Mined! ***" }
+            $this.chain += $block
+            
+        }     
+        #Clear the pending transactions because they are no longer pending
+        $this.pendingTransactions = @()
+        $this.pendingTransactions += [Transaction]::new("rewardAddress", $minningRewardAddress, $this.minningReward)
     }
 
-    createTransaction([Transaction]$transaction){
-        if ($this.pendingTransactions.length -eq 0) { $this.pendingTransactions = $transaction }
-        elseif($this.pendingTransactions.length -gt 0) { $this.pendingTransactions += $transaction }
+    createTransaction([String]$fromAddress, [String]$toAddress, [Int]$ammount){
+        $this.pendingTransactions += $([Transaction]::new($fromAddress, $toAddress, $ammount))
     }
 
     [Block] getLatestBlock() {
@@ -83,11 +94,12 @@ class BlockChain {
     [Int32] getBallanceOfAddress($address){
         $ballance = 0
 
-        foreach ($block in $($this.chain)) {
-            foreach ($transaction in $block.transaction) {
-                if ($transaction.fromAddress -eq $address){ $ballance -= $transaction.ammount }
-                if ($transaction.toAddress -eq $address) { $ballance += $transaction.ammount }
-            }
+        foreach ($block in $this.chain) {
+            #foreach ($transaction in $block.transaction) { # This is the line with the error for the calculation
+                # We need to make transactions an array or figure out some way to itterate through all the transactions
+                if ($block.transaction.fromAddress -eq $address){ $ballance -= $($block.transaction.ammount) }
+                elseif ($block.transaction.toAddress -eq $address) { $ballance += $($block.transaction.ammount) }
+            #}
         }
         return $ballance
     }
@@ -107,14 +119,22 @@ class BlockChain {
         return $True
     }
 }
-
+# Create the initial block chain with a blank genesis block
 $blockChain = [BlockChain]::new()
-$blockChain.createTransaction($([Transaction]::new("address1","address2",100)))
-$blockChain.createTransaction($([Transaction]::new("address2","address1",50)))
 
-$blockChain.minePendingTransactions("address3")
-$blockChain.minePendingTransactions("address3")
+# Create some fake transactions
+$blockChain.createTransaction("rewardAddress","address1",100)
+$blockChain.createTransaction("address1","address2",100)
+$blockChain.createTransaction("address2","address1",50)
 
-Write-Host "Ballance of address3 $($blockChain.getBallanceOfAddress("address3"))"
-Write-Host "Ballance of address2 $($blockChain.getBallanceOfAddress("address2"))"
-Write-Host "Ballance of address1 $($blockChain.getBallanceOfAddress("address1"))"
+# Mine the transactions and show the output
+$blockChain.minePendingTransactions("miner")
+Write-Host "Ballance of address2: $($blockChain.getBallanceOfAddress("address2"))"
+Write-Host "Ballance of address1: $($blockChain.getBallanceOfAddress("address1"))"
+
+# Mine the transactions from when the miner mined the previous transactions
+# The miner does not get his reward till his transaction has been processed
+$blockChain.minePendingTransactions("miner")
+Write-Host "Ballance of miner: $($blockChain.getBallanceOfAddress("miner"))"
+Write-Host "Ballance of address2: $($blockChain.getBallanceOfAddress("address2"))"
+Write-Host "Ballance of address1: $($blockChain.getBallanceOfAddress("address1"))"
