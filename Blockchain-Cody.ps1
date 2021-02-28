5fd575b965f833dc6a1d85cee107dbddb100f2e0
WerkmeisterCody <79776112+WerkmeisterCody@users.noreply.github.com>
2021-02-27 21:48:17 -0500

    Comit

---
+++ Blockchain-Cody.ps1
@@ -0,0 +1,47 @@
+class Transaction { 
+    [String]$fromAddress;
+    [String]$toAddress;
+    [Int32]$ammount;
+
+    Transaction ([String]$fromAddress, [String]$toAddress, [Int32]$ammount) {
+        $this.fromAddress =$fromAddress
+        $this.toAddress = $fromAddress
+        $this.ammount = $ammount
+    }
+}
+
+class Block {
+    [Int32] $nonce;
+    [string] $hash;
+    [string] $hash_prev;
+    [Transaction] $transaction;
+
+    Block([Transaction]$transaction, [String]$hash_prev) {
+        $this.nonce = 0
+        $this.hash = calculateHash
+        $this.hash_prev = Hash_prev
+        $this.transaction = $transaction
+    }
+    [String] calculateHash(){
+      $  
+    }
+}
+
+class BlockChain {
+     $chain =@() 
+
+     Blockchain(){
+         $this.chain = createGenesisBlock
+     }
+
+     [Block] createGenesisBlock(){
+        return [BLock]::new()
+     }
+     
+     addBlock ([Block]$newblock){
+         $this.chain.add($newBlock)
+     }
+     [Block] getLatestBlock() {
+         return $this.chain[$this.chain.length - 1]
+     }
+}
